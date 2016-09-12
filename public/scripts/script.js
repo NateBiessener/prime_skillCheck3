afbb451fad7f79f4d8c6c091d32d4f272486bd94
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-12 14:38:42 -0500

    add form fields and send to server

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,16 +2,18 @@ console.log('script sourced');
 
 $(document).ready(function() {
   console.log('jq working');
-  $.ajax({
-    url: '/',
-    type: 'POST',
-    data: {
-      whoseJoke: 'Dude',
-      jokeQuestion: 'fjdkslafjkds',
-      punchLine: 'fdfdas'
-    },
-    success: function(data){
-      console.log(data);
-    }
-  })
+  $('#submitJoke').on('click', function(){
+    $.ajax({
+      url: '/',
+      type: 'POST',
+      data: {
+        whoseJoke: $('#nameIn').val(),
+        jokeQuestion: $('#questionIn').val(),
+        punchLine: $('#funnyIn').val()
+      },
+      success: function(data){
+        console.log(data);
+      }//end success callback
+    });//end ajax call
+  });//end submitJoke onclick
 })
