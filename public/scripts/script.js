7592a1fc64ebb78e9493c792b021f7beb470921b
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-12 14:52:08 -0500

    update display

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -13,7 +13,11 @@ $(document).ready(function() {
       },
       success: function(data){
         console.log(data);
+        for (var i = 0; i < data.length; i++) {
+          var joke = '<div class="joke" style="display: inline-block"><p class="question">' + data[i].jokeQuestion + '</p><p class="funny">' + data[i].punchLine + '</p><p class="author">Submitted by - ' + data[i].whoseJoke + '</p></div>';
+          $('#outDiv').append(joke);
+        }
       }//end success callback
     });//end ajax call
   });//end submitJoke onclick
-})
+})//end doc ready
