954fa5c2ca4ba36765319dcdeca72f1ee184558a
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-12 15:53:48 -0500

    one joke at a time

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -22,18 +22,28 @@ $(document).ready(function() {
       type: 'GET',
       data: {},
       success: function(data){
+        $('#outDiv').empty();
         for (var i = 0; i < data.length; i++) {
-          var joke = '<div class="joke" style="display: none"><p class="question">' + data[i].jokeQuestion + '</p><p class="funny">' + data[i].punchLine + '</p><p class="author">Submitted by - ' + data[i].whoseJoke + '</p></div>';
+          var joke = '<div class="joke"><p class="question" style="display: none">' + data[i].jokeQuestion + '</p><p class="funny" style="display: none">' + data[i].punchLine + '</p><p class="author" style="display: none">Submitted by - ' + data[i].whoseJoke + '</p><button type="button" class="nextJoke" style="display: none" onclick="showJokes(' + (i + 1) + ')">Next Joke</button></div>';
           $('#outDiv').append(joke);
         };
-        showJokes();
+        showJokes(0);
       }
     })
   })
 })//end doc ready
 
-var showJokes = function(){
-  $('.joke').each(function(){
-    $(this).fadeIn();
-  })
+var showJokes = function(joke){
+  if (joke > $('.joke').length - 1){
+    alert('No more jokes! Add some more!');
+    return;
+  }
+  console.log('in showJokes');
+  $($('.joke')[joke]).children('.question').fadeIn(400, function(){
+    $(this).parent().children('.funny').fadeIn(400, function(){
+      $(this).parent().children('.author').fadeIn(400, function(){
+        $(this).parent().children('.nextJoke').fadeIn();
+      });
+    });
+  });
 }
